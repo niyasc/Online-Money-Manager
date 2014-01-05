246e9f8c6d0fb389d710d245b8424907af973512
Niyas C <niyasc@localhost.localdomain>
2014-01-05 08:49:17 +0530

    Work completed for current session

--- html/members.php
+++ html/members.php
@@ -67,10 +67,10 @@
     			$start=$start[0]["start"];
     			$end=$end[0]["end"];
     		}
-    			$records=query("select Item,Day,Expense from expense where User=(select id from accounts where uname=?) and Day>=? and Day<=? order by Day asc",$_SESSION["uname"],$start,$end);
+    			$records=query("select Item,Day,Expense,Category from expense where User=(select id from accounts where uname=?) and Day>=? and Day<=? order by Day asc",$_SESSION["uname"],$start,$end);
     		}
-    		
-    		render("show_records.php",["title"=>"Show Records","records"=>$records,"from"=>$start,"to"=>$end]);
+    		$categories=query("select category from categories where uid=(select id from accounts where uname=?)",$_SESSION["uname"]);
+    		render("show_records.php",["title"=>"Show Records","records"=>$records,"from"=>$start,"to"=>$end,"categories"=>$categories]);
     	}
     	else
     	{
