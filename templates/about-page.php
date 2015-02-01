de56947a6e0ac27595ba18ae28805aadcb13d894
Niyas Chirayakuth <niyasc@localhost.localdomain>
2015-02-01 20:13:44 +0530

    About page updated

--- templates/about-page.php
+++ templates/about-page.php
@@ -1,5 +1,5 @@
 <div class='content'>
-<table class='table table-bordered table-striped' style=" margin-left:auto;margin-right:auto;height:100%">
+<table class='table table-bordered table-striped' style=" margin-left:auto;margin-right:auto;">
 	<tr>
 		<td >
 			<table class='table table-bordered table-striped' style="width:100%; margin-left:auto;margin-right:auto">
@@ -17,70 +17,6 @@
 							My profile is my page in github.<br/>
 							It is here
 							<a href="http://github.com/niyasc" target='_blank'>github/niyasc</a>
-					</p>
-
-					</td>
-				</tr>
-				<tr>
-					<td >
-						<h2 style="text-align:center">
-						Moin Khan
-						</h2>
-					</td>
-				</tr>
-				<tr>
-					<td>
-					
-						<p style='text-align:center'>
-							Description
-					</p>
-
-					</td>
-				</tr>
-				<tr>
-					<td >
-						<h2 style="text-align:center">
-						Shadab Ahmad
-						</h2>
-					</td>
-				</tr>
-				<tr>
-					<td>
-					
-						<p style='text-align:center'>
-							Tamhid Khan
-					</p>
-
-					</td>
-				</tr>
-				<tr>
-					<td >
-						<h2 style="text-align:center">
-						Zishan Ahmed
-						</h2>
-					</td>
-				</tr>
-				<tr>
-					<td>
-					
-						<p style='text-align:center'>
-							Description
-					</p>
-
-					</td>
-				</tr>
-				<tr>
-					<td >
-						<h2 style="text-align:center">
-						Moin Khan
-						</h2>
-					</td>
-				</tr>
-				<tr>
-					<td>
-					
-						<p style='text-align:center'>
-							Description
 					</p>
 
 					</td>
